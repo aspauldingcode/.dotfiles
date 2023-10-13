91ea4b33c29d8ce52300d5802e32bf58cef43422
aspauldingcode <aspauldingcode@gmail.com>
2023-10-13 08:48:52 -0600

    Updating .dotfiles from NIXSTATION64

--- system/NIXSTATION64/packages-configuration.nix
+++ system/NIXSTATION64/packages-configuration.nix
@@ -29,5 +29,31 @@
 			cargo nodePackages_latest.npm
 			perl 
 			hexedit virt-manager
+			#rebuild
+			(pkgs.writeShellScriptBin "rebuild" ''
+			# NIXSTATION64(x86_64-linux)
+			cd ~/.dotfiles
+			sudo nixos-rebuild switch --flake .#NIXSTATION64 
+			home-manager switch --flake .#alex@NIXSTATION64
+			'')
+			#update
+			(pkgs.writeShellScriptBin "update" ''
+			# Navigate to the Repository Directory:
+			cd ~/.dotfiles
+
+			#Fetch the Latest Changes:
+			git fetch
+
+			# Update Your Local Branch:
+			git checkout main
+			git merge origin/main
+
+			# Commit Your Changes (if needed):
+			git add .
+			git commit -m "Updating .dotfiles from NIXSTATION64"
+
+			# Push the Changes to the Remote Repository:
+			git push origin main
+			'')
 			]; 
 }
