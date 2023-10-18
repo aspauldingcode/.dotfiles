bbf010586f3015a137e58533712183850b30f38f
Alex Spaulding <aspauldingcode@gmail.com>
2023-10-18 12:52:58 -0600

    Updating .dotfiles from NIXY

--- system/NIXY/darwin-configuration.nix
+++ system/NIXY/darwin-configuration.nix
@@ -4,6 +4,11 @@
 {
 imports = [ 
 ./yabai.nix
+./skhd.nix
+./spacebar.nix
+./defaults-macos.nix
+#./homebrew-pkgs.nix #FIXME
+
 ];
 	nixpkgs = {
 		hostPlatform = "aarch64-darwin";
@@ -47,7 +52,7 @@ imports = [
 		home-manager switch --flake .#alex@NIXY
 		'')
 		#update
-		(pkgs.writeShellScriptBin "update" ''
+sp		(pkgs.writeShellScriptBin "update" ''
 		# Navigate to the Repository Directory:
 		cd ~/.dotfiles
 
@@ -66,137 +71,10 @@ imports = [
 		git push origin main
 		'')
 	];
-	services.skhd = {
-    enable = true;
-    package = pkgs.skhd;
-    skhdConfig = ''
-			alt - return : open -n /Applications/Alacritty.app;
 
-			alt - h : yabai -m window --focus west
-			alt - j : yabai -m window --focus south
-			alt - k : yabai -m window --focus north
-			alt - l : yabai -m window --focus east
-			alt - left  : yabai -m window --focus west
-			alt - down  : yabai -m window --focus south
-			alt - up    : yabai -m window --focus north
-			alt - right : yabai -m window --focus east
 
-# shift window in current workspace
-			alt + shift - h : yabai -m window --swap west || $(yabai -m window --display west; yabai -m display --focus west)
-			alt + shift - j : yabai -m window --swap south || $(yabai -m window --display south; yabai -m display --focus south)
-			alt + shift - k : yabai -m window --swap north || $(yabai -m window --display north; yabai -m display --focus north)
-			alt + shift - l : yabai -m window --swap east || $(yabai -m window --display east; yabai -m display --focus east)
-			alt + shift - left : yabai -m window --swap west || $(yabai -m window --display west; yabai -m display --focus west)
-			alt + shift - down : yabai -m window --swap south || $(yabai -m window --display south; yabai -m display --focus south)
-			alt + shift - up : yabai -m window --swap north || $(yabai -m window --display north; yabai -m display --focus north)
-			alt + shift - right : yabai -m window --swap east || $(yabai -m window --display east; yabai -m display --focus east)
-# set insertion point in focused container
-			alt + ctrl - h : yabai -m window --insert west
-			alt + ctrl - j : yabai -m window --insert south
-			alt + ctrl - k : yabai -m window --insert north
-			alt + ctrl - l : yabai -m window --insert east
-			alt + ctrl - left  : yabai -m window --insert west
-			alt + ctrl - down  : yabai -m window --insert south
-			alt + ctrl - up    : yabai -m window --insert north
-			alt + ctrl - right : yabai -m window --insert east
 
-# go back to previous workspace (kind of like back_and_forth in i3)
-			alt - b : yabai -m space --focus recent
 
-# move focused window to previous workspace
-			alt + shift - b : yabai -m window --space recent; \
-			yabai -m space --focus recent
-
-# move focused window to next/prev workspace
-			alt + shift - 1 : yabai -m window --space 1
-			alt + shift - 2 : yabai -m window --space 2
-			alt + shift - 3 : yabai -m window --space 3
-			alt + shift - 4 : yabai -m window --space 4
-			alt + shift - 5 : yabai -m window --space 5
-			alt + shift - 6 : yabai -m window --space 6
-			alt + shift - 7 : yabai -m window --space 7
-			alt + shift - 8 : yabai -m window --space 8
-			alt + shift - 9 : yabai -m window --space 9
-			alt + shift - 0 : yabai -m window --space 10
-
-			alt + shift - y : yabai -m space --mirror y-axis
-			alt + shift - x : yabai -m space --mirror x-axis
-
-# balance size of windows
-			alt + shift - 0 : yabai -m space --balance
-			alt - e : yabai -m space --layout bsp
-			alt - l : yabai -m space --layout float
-			alt - s : yabai -m space --layout stack
-			
-# toggle borders
-	#alt - y : yabai -m window --toggle border
-
-# cycle through stack windows
-			alt - p : yabai -m window --focus stack.next || yabai -m window --focus south
-			alt - n : yabai -m window --focus stack.prev || yabai -m window --focus north
-
-# forwards
-			alt - p : yabai -m query --spaces --space \
-			| jq -re ".index" \
-			| xargs -I{} yabai -m query --windows --space {} \
-			| jq -sre "add | map(select(.minimized != 1)) | sort_by(.display, .frame.y, .frame.x, .id) | reverse | nth(index(map(select(.focused == 1))) - 1).id" \
-			| xargs -I{} yabai -m window --focus {}
-
-# backwards
-		alt - n : yabai -m query --spaces --space \
-			| jq -re ".index" \
-			| xargs -I{} yabai -m query --windows --space {} \
-			| jq -sre "add | map(select(.minimized != 1)) | sort_by(.display, .frame.y, .frame.y, .id) | nth(index(map(select(.focused == 1))) - 1).id" \
-			| xargs -I{} yabai -m window --focus {}
-
-			alt + shift - q : yabai -m window --close
-			alt - f : yabai -m window --toggle zoom-fullscreen
-			alt + shift - f : yabai -m window --toggle native-fullscreen
-			echo "skhd config loaded...
-			'';
-	};
-
-	services.spacebar = {
-		enable = true;
-		package = pkgs.spacebar;
-		config = {
-
-		}; 
-		extraConfig = ''
-			spacebar -m config right_shell off
-			spacebar -m config position                    top
-			spacebar -m config height                      26
-			spacebar -m config title                       on
-			spacebar -m config spaces                      on
-			spacebar -m config clock                       on
-			spacebar -m config power                       on
-			spacebar -m config padding_left                20
-			spacebar -m config padding_right               20
-			spacebar -m config spacing_left                25
-			spacebar -m config spacing_right               15
-			spacebar -m config text_font                   "Helvetica Neue:Bold:12.0"
-			spacebar -m config icon_font                   "Font Awesome 5 Free:Solid:12.0"
-			spacebar -m config background_color            0x00000000
-			spacebar -m config foreground_color            0xffa8a8a8
-			spacebar -m config power_icon_color            0xffcd950c
-			spacebar -m config battery_icon_color          0xffd75f5f
-			spacebar -m config dnd_icon_color              0xffa8a8a8
-			spacebar -m config clock_icon_color            0xffa8a8a8
-			spacebar -m config power_icon_strip             
-			spacebar -m config space_icon                  •
-			spacebar -m config space_icon_color            0xffffab91
-			spacebar -m config space_icon_color_secondary  0xff78c4d4
-			spacebar -m config space_icon_color_tertiary   0xfffff9b0
-			spacebar -m config space_icon_strip            1 2 3 4 5 6 7 8 9 10
-			spacebar -m config clock_icon                  
-			spacebar -m config dnd_icon                    
-			spacebar -m config clock_format                "%d/%m/%y %R"
-			spacebar -m config right_shell                 on
-			spacebar -m config right_shell_icon            
-			spacebar -m config right_shell_command         "whoami"
-			echo "spacebar configuration loaded.."
-			'';
-	};
 #enable brew packages just in case
 	homebrew.enable = true;
 
@@ -246,50 +124,6 @@ imports = [
 			font-awesome_5
 	];
 
-##SYSTEM DEFAULTS!!!!
-
-# Hide Desktop icons
-	system.defaults.finder.CreateDesktop = false;
-# Show all files
-	system.defaults.finder.AppleShowAllFiles = true;
-# Show all extentions
-	system.defaults.finder.AppleShowAllExtensions = true;
-# Change the default search scope. Use "SCcf" to default to the current folder.
-	system.defaults.finder.FXDefaultSearchScope = "SCcf";
-# Whether to show warnings when change the file extension of files. The default is true.
-	system.defaults.finder.FXEnableExtensionChangeWarning = false;
-# Change the default finder view. “icnv” = Icon view, “Nlsv” = List view, “clmv” = Column View, “Flwv” = Gallery View The default is icnv.
-	system.defaults.finder.FXPreferredViewStyle = "Nlsv";
-#Whether to allow quitting of the Finder. The default is false.
-	system.defaults.finder.QuitMenuItem = true;
-#Show path breadcrumbs in finder windows. The default is false.
-	system.defaults.finder.ShowPathbar = false;
-#Show status bar at bottom of finder windows with item/disk space stats. The default is false.
-	system.defaults.finder.ShowStatusBar = false;
-#Whether to show the full POSIX filepath in the window title. The default is false.
-	system.defaults.finder._FXShowPosixPathInTitle = true;
-#Disables the ability for a user to access the console by typing “>console” for a username at the login window. Default is false.
-	system.defaults.loginwindow.DisableConsoleAccess = true;
-#Allow users to login to the machine as guests using the Guest account. Default is true.
-	system.defaults.loginwindow.GuestEnabled = false;
-
-#Text to be shown on the login window. Default is “\\U03bb”.
-#system.defaults.loginwindow.LoginwindowText = "Login as: " whoami; ### READ HOMEMANAGER CONFIG!!!
-
-#If set to true, the Power Off menu item will be disabled when the user is logged in. Default is false.
-	system.defaults.loginwindow.PowerOffDisabledWhileLoggedIn = false;
-#Hides the Restart button on the login screen. Default is false.
-	system.defaults.loginwindow.RestartDisabled = false;
-#Disables the “Restart” option when users are logged in. Default is false.
-	system.defaults.loginwindow.RestartDisabledWhileLoggedIn = false;
-# Hides the Sleep button on the login screen. Default is false.
-	system.defaults.loginwindow.SleepDisabled = false;
-#Whether to enable smooth scrolling. The default is true.
-	system.defaults.NSGlobalDomain.NSScrollAnimationEnabled = false;
-#Whether to animate opening and closing of windows and popovers. The default is true.
-	system.defaults.NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = false;
-#Sets the speed speed of window resizing. The default is given in the example.
-	system.defaults.NSGlobalDomain.NSWindowResizeTime = 0.0;
 
 # system.build = builtins.exec "echo 'hello, world.'";
 
