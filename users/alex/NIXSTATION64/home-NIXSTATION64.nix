b86dd7d20de453c0185ffb4f7ab788cd1fa2d2c6
aspauldingcode <aspauldingcode@gmail.com>
2023-10-21 14:19:56 -0600

    Updating .dotfiles from NIXSTATION64

--- users/alex/NIXSTATION64/home-NIXSTATION64.nix
+++ users/alex/NIXSTATION64/home-NIXSTATION64.nix
@@ -2,44 +2,44 @@
 
 {
 #import other home-manager modules which are NIXSTATION64-specific
-	imports = [
-		./packages-NIXSTATION64.nix 
-		./nvim.nix
-	]; 
-	home = {
-		username = "alex";
-		homeDirectory = "/home/alex";
-		stateVersion = "23.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-			file = { # MANAGE DOTFILES?
-			};
-	};
+imports = [
+  ./packages-NIXSTATION64.nix 
+  ./nvim.nix
+]; 
+home = {
+  username = "alex";
+  homeDirectory = "/home/alex";
+  stateVersion = "23.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  file = { # MANAGE DOTFILES?
+};
+    };
 
-	services = {
-		mako = {
-			enable = true;
-			maxVisible = -1;
-			output = "DP-2";
-			layer = "overlay";
-			anchor = "top-right";
-			#font = 
-			borderSize = 2;
-			borderColor = "#A34A28";
-			borderRadius = 10;
-			defaultTimeout = 5000;
-			ignoreTimeout = false;
-		};
-		
-	};
-	programs = {
-		git = {
-			enable = true;
-			userName  = "aspauldingcode";
-			userEmail = "aspauldingcode@gmail.com";
-		};
-	};
+    services = {
+      mako = {
+        enable = true;
+        maxVisible = -1;
+        output = "DP-2";
+        layer = "overlay";
+        anchor = "top-right";
+            #font = 
+            borderSize = 2;
+            borderColor = "#A34A28";
+            borderRadius = 10;
+            defaultTimeout = 5000;
+            ignoreTimeout = false;
+          };
+
+        };
+        programs = {
+          git = {
+            enable = true;
+            userName  = "aspauldingcode";
+            userEmail = "aspauldingcode@gmail.com";
+          };
+        };
 
 # Decoratively fix virt-manager error: "Could not detect a default hypervisor" instead of imperitively through virt-manager's menubar > file > Add Connection
-	dconf.settings = {
+dconf.settings = {
   "org/virt-manager/virt-manager/connections" = {
     autoconnect = ["qemu:///system"];
     uris = ["qemu:///system"];
@@ -47,5 +47,5 @@
 };
 
 # Nicely reload system units when changing configs
-		systemd.user.startServices = "sd-switch"; # TODO: UPDATE IF USING DIFFERENT BOOTLOADER!
-	}
+systemd.user.startServices = "sd-switch"; # TODO: UPDATE IF USING DIFFERENT BOOTLOADER!
+    }
--- users/alex/extraConfig/fish.nix
+++ users/alex/extraConfig/fish.nix
@@ -13,12 +13,12 @@ home-manager.users.alex = {
       /*{
       name = "z";
       src = pkgs.fetchFromGitHub {
-        owner = "jethrokuan";
-        repo = "z";
-        rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
-        sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+      owner = "jethrokuan";
+      repo = "z";
+      rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+      sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
       };*/
-    	];
-    };
+    ];
+  };
 };
 
