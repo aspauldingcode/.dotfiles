78bd29acba04af04f9f077b41bceb1e8f676dd60
aspauldingcode <aspauldingcode@gmail.com>
2024-06-06 22:50:03 -0600

    added more menubar item osascript menu plugin thingies

---
+++ users/alex/NIXY/sketchybar/plugins/open_menubar_items.sh
@@ -0,0 +1,45 @@
+osascript -e 'tell application "System Events" to tell process "macOS InstantView"
+    click menu bar item 1 of menu bar 2
+end tell
+'
+
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "UnnaturalScrollWheels"
+    click menu bar item 1 of menu bar 2
+end tell
+'
+
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "AltTab"
+    click menu bar item 1 of menu bar 2
+end tell
+'
+
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "Karabiner-Menu"
+    click menu bar item 1 of menu bar 1
+end tell
+'
+
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "Background Music"
+    click menu bar item 1 of menu bar 1
+end tell
+'
+
+sleep 1
+
+osascript -e 'tell application "System Events" to tell process "Flameshot"
+    click menu bar item 1 of menu bar 2
+end tell
+'
+
+osascript -e 'tell application "System Events" to tell process "Spotlight"
+    click menu bar item 1 of menu bar 2
+end tell
+'
+
