6b8e2b053a5a7153142e393ebd52641a2aa4110a
aspauldingcode <aspauldingcode@gmail.com>
2024-07-12 13:57:51 -0700

    more fixes/changes to sketchybar

--- users/alex/NIXY/sketchybar/plugins/add_spaces_sketchybar.sh
+++ users/alex/NIXY/sketchybar/plugins/add_spaces_sketchybar.sh
@@ -6,6 +6,14 @@ source "$HOME/.config/sketchybar/colors.sh"
 source "$HOME/.config/sketchybar/icons.sh"
 source "$PLUGIN_DIR/detect_arch_and_source_homebrew_packages.sh"
 
+reorder_space_items() {
+    # Get all space items and sort them numerically
+    local space_items=$(sketchybar --query bar | $jq -r '.items[] | select(startswith("space."))' | sort -V)
+
+    # Reorder space items numerically
+    sketchybar --reorder $space_items
+}
+
 function update_sketchybar_spaces() {
     # Get the full label from the function and extract only the numeric part
     full_label=$(source $PLUGIN_DIR/sway_spaces.sh; check_current_active_space_label)
@@ -16,26 +24,37 @@ function update_sketchybar_spaces() {
 
     sketchybar_spaces=($(sketchybar --query bar | $jq -r '.items[] | select(startswith("space."))'))
 
-for space in "${relevant_spaces[@]}"; do
-    label=$(echo "$space" | /usr/bin/sed 's/^_//')  # Ensure no leading underscore
-    color="$WHITE"  # Default color
-    [[ "$label" == "$active_space_label" ]] && color="$ORANGE"  # Set color to ORANGE if it's the active space
+    # Create an associative array for space colors
+    declare -A space_colors
 
-    # Set the properties of the cloned space item
-    sketchybar --set space.$label \
-                    label="$label" \
-                    label.color="$color" \
-                    click_script="$yabai -m space --focus _$label" \
-                    padding_left=5 \
-                    padding_right=5 \
-		            drawing=on
-                    
-    # Clone the existing space item and rename the cloned item
-    sketchybar --clone space.$label space after
+    # Set default color for all spaces and special color for active space
+    for space in "${relevant_spaces[@]}"; do
+        space_colors[$space]="$WHITE"
+    done
+    space_colors[$active_space_label]="$ORANGE"
 
-    # Remove the original space item from the list of spaces
-    sketchybar_spaces=("${sketchybar_spaces[@]/space.$label/}")
-done
+    # Prepare the command string
+    command_string=""
+
+    # Build the command string for all spaces
+    for space in "${relevant_spaces[@]}"; do
+        label=${space#_}  # Remove leading underscore if present
+        color=${space_colors[$label]}
+
+        command_string+="--set space.$label label=$label padding_right=5 padding_left=5 label.color=$color "
+        command_string+="click_script=\"$yabai -m space --focus _$label\" "
+        command_string+="padding_left=5 padding_right=5 drawing=on "
+        command_string+="--clone space.$label space after "
+
+        # Remove the space from sketchybar_spaces array
+        sketchybar_spaces=("${sketchybar_spaces[@]/space.$label/}")
+    done
+
+    # Execute the command
+    sketchybar $command_string
+    
+    # Call the function to reorder space items
+    reorder_space_items
 
     # Remove any remaining stale space items
     for space_id in "${sketchybar_spaces[@]}"; do
@@ -61,17 +80,9 @@ done
     echo -e "\nPrint Space Labels:"
     read_all_spaces
 
-    # echo -e "\nrelevent spaces: \n$relevant_spaces"
-
     echo $active_space_label
-    # check_current_active_display
-    # check_current_active_display_label
-
-    # check_current_active_space
-    # check_current_active_space_label
 
     echo -e "\nReordering space items"
-    reorder_space_items
 }
 
 echo -e "\n\n\nRunning update_sketchybar_spaces\n\n\n"
