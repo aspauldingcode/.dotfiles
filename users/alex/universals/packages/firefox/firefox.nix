55be83889af32aafe1723367ab12df948c219d97
aspauldingcode <aspauldingcode@gmail.com>
2024-07-18 15:18:13 -0700

    Firefox on nIXY compiled?

--- users/alex/universals/packages/firefox/firefox.nix
+++ users/alex/universals/packages/firefox/firefox.nix
@@ -56,14 +56,12 @@ in
             version = "1.6.7";
             addonId = "myallychou@gmail.com";
             url = "https://addons.mozilla.org/en-US/firefox/addon/youtube-recommended-videos/";
-            sha256 = "sha256-o3RC/5xO7XRF8N6v0DpO8RXVWidli5/xgRgSp/QJJZI=";
+            sha256 = "sha256-OxxcF2e/TXDY9aXkADmnzxtrPk+80+1RQ1DKz29VwhQ=";
             meta = {
               homepage = "https://unhook.app/";
               description = "Hide YouTube distractions including related videos, comments, suggestions, and trending content.";
               license = lib.licenses.unfree;
-              mozPermissions = [
-                "storage"
-              ];
+              mozPermissions = [ "storage" ];
               platforms = lib.platforms.all;
             };
           })
@@ -73,14 +71,12 @@ in
             version = "2.5.0";
             addonId = "{17c4514d-71fa-4633-8c07-1fe0b354c885}";
             url = "https://addons.mozilla.org/en-US/firefox/addon/hide-youtube-thumbnails/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search.Check";
-            sha256 = "sha256-F4n1Fm0LSlKSS4Mu2C9EVeMqfIhFtZKie5wYj72DsBw=";
+            sha256 = "sha256-fBNIOfI/DktTBUUxamtqAMTfwYFBF7xPLIyoJyUa/OE=";
             meta = {
               homepage = "https://github.com/domdomegg/hideytthumbnails-extension";
               description = "A simple browser extension which removes thumbnails from YouTube, for less clickbaity browsing.";
               license = lib.licenses.mit;
-              mozPermissions = [
-                "storage"
-              ];
+              mozPermissions = [ "storage" ];
               platforms = lib.platforms.all;
             };
           })
