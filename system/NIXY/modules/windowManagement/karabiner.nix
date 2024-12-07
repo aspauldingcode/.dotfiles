ffaad549a665569f954d4c52bea4fc63f13ac3c2
Alex S. <aspauldingcode@gmail.com>
2024-12-07 12:19:15 -0700

    Update karabiner.nix

--- system/NIXY/modules/windowManagement/karabiner.nix
+++ system/NIXY/modules/windowManagement/karabiner.nix
@@ -1,8 +1,9 @@
 { config, pkgs, ... }:
 
+#FIXME: FAILS DUE TO https://github.com/LnL7/nix-darwin/issues/1041
 {
   services.karabiner-elements = {
-    enable = true; # Whether to enable Karabiner-Elements.
+    enable = false; # Whether to enable Karabiner-Elements. 
   };
 
   # configuration for karabiner is in home-manager.
