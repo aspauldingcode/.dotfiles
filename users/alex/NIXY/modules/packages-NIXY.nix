607c407032377ee22d441ab486866b167631a53a
Alex S. <aspauldingcode@gmail.com>
2024-12-07 14:39:13 -0700

    Update packages-NIXY.nix no rsync home-manager issue

--- users/alex/NIXY/modules/packages-NIXY.nix
+++ users/alex/NIXY/modules/packages-NIXY.nix
@@ -22,19 +22,6 @@ let
   inherit (config.colorScheme) palette;
 in
 {
-  # Copy Home-Manager Nix GUI apps to ~/Applications on darwin:
-  home.activation = {
-    rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-      apps_source="$genProfilePath/home-path/Applications"
-      moniker="Home Manager Trampolines"
-      app_target_base="${config.home.homeDirectory}/Applications"
-      app_target="$app_target_base/$moniker"
-      mkdir -p "$app_target"
-      ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
-    '';
-  };
-
   nixpkgs = {
     config = {
       allowUnfree = true;
