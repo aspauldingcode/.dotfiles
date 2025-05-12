196bf5b1b088c8bc67a0d115c3f61806f3dc489b
aspauldingcode <aspauldingcode@gmail.com>
2025-05-12 11:50:10 -0700

    successful rebuild NIXY

--- flake.nix
+++ flake.nix
@@ -270,6 +270,13 @@
                   inherit (final) system config;
                 };
               })
+              (final: prev: {
+                nodejs = prev.nodejs_22;
+                nodejs-slim = prev.nodejs-slim_22;
+
+                nodejs_20 = prev.nodejs_22;
+                nodejs-slim_20 = prev.nodejs-slim_22;
+              })
             ];
           };
           specialArgs = commonSpecialArgs;
